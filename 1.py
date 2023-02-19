2bba0ec840a3253b9a958bae45e00cc4e2ed73e2
AAA <alextun5@mail.ru>
2023-02-19 23:02:40 +0300

    ready project

--- 1.py
+++ 1.py
@@ -1,29 +1,46 @@
 from pprint import pprint
+from random import randint
 
 text_name = 'pushkin123.txt'
 stat = {}
 # stat = {'a':{'t': 2, 'u': 4}, 'b':{'t': 2, 'u': 4}}
-prev_char = ' '
+sequence = '    '
 with open(text_name, mode='r', encoding='utf8') as file:
     for line in file:
         for char in line:
-            if prev_char in stat:
-                if char in stat[prev_char]:
-                    stat[prev_char][char] += 1
+            if sequence in stat:
+                if char in stat[sequence]:
+                    stat[sequence][char] += 1
                 else:
-                    stat[prev_char][char] = 1
+                    stat[sequence][char] = 1
             else:
-                stat[prev_char] = {char: 1}
-            prev_char = char
+                stat[sequence] = {char: 1}
+            sequence = sequence[1:] + char
 
-# pprint(stat)
+pprint(stat)
 totals = {}
 stat_for_generation = {}
-for prev_char, char_stat in stat.items():
-    totals[prev_char] = 0
-    stat_for_generation[prev_char] = []
+for sequence, char_stat in stat.items():
+    totals[sequence] = 0
+    stat_for_generation[sequence] = []
     for chars, count in char_stat.items():
-        totals[prev_char] += count
-        stat_for_generation[prev_char].append([count, chars])
-    stat_for_generation[prev_char].sort()
-    stat_for_generation[prev_char].reverse()
+        totals[sequence] += count
+        stat_for_generation[sequence].append([count, chars])
+    stat_for_generation[sequence].sort()
+    stat_for_generation[sequence].reverse()
+
+# pprint(stat_for_generation)
+number_of_chars = 500
+counter = 0
+sequence = '    '
+while counter <= number_of_chars:
+    char_stat = stat_for_generation[sequence]
+    pos = 0
+    dice = randint(1, totals[sequence])
+    for count, char in char_stat:
+        pos += count
+        if dice <= pos:
+            break
+    print(char, end='')
+    sequence = sequence[1:] + char
+    counter += 1
